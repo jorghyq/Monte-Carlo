df6f173a764ef277055683b01dc23c143214e271
jorghyq <jorghyq@gmail.com>
2015-11-05 11:09:35 +0100

    ha

--- mc/elements.py
+++ mc/elements.py
@@ -1,1 +1,11 @@
 #class elements will be defined here
+
+class Elements:
+    def __init__(self, num, symbol):
+        self.num = num
+        self.symbol = symbol
+
+    def set_shape(self, shape):
+        # active site = 2, non-active = 1
+        pass
+
--- mc/solver.py
+++ mc/solver.py
@@ -1,7 +1,21 @@
 # main class solver will be defined here
-
+import sys
 
 
 class Solver():
-    def __init__(self):
+    def __init__(self, elements):
+        # default settings of the lattice
+        self.length = 20
+        self.width = 20
+        self.MAX_RUN = 100000
+        self.elements = elements
 
+    def init(self):
+        # TODO: initialize the elements
+
+    def step(self, step_num):
+        if step_num <= 0:
+            print "step number should be positve integer"
+        else:
+            while step_num > 0:
+                pass
